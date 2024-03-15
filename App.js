d3805ef074ab8a42c7bfc0c6eff29918463e8f63
tarurata <saycheese5963@gmail.com>
2024-03-15 14:04:36 -0700

    Finished handle platform differences.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  View } from 'react-native';
+import { StyleSheet,  View, Platform } from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from 'react-native-view-shot';
 
@@ -11,6 +11,7 @@ import IconButton from './components/IconButton'
 import EmojiPicker from './components/EmojiPicker'
 import EmojiList from  './components/EmojiList'
 import EmojiSticker from  './components/EmojiSticker'
+import domtoimage from 'dom-to-image'
 
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
@@ -51,19 +52,36 @@ export default function App() {
     };
 
     const onSaveImageAsync = async () => {
-        try {
-            const localUri = await captureRef(imageRef, {
-                height: 440,
-                quality: 1,
-            });
-            await MediaLibrary.saveToLibraryAsync(localUri);
-            if (localUri) {
-                alert("Saved!");
+        if (Platform.OS !== 'web') {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert("Saved!");
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
+        } else {
+            try {
+                const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    width: 320,
+                    height: 440,
+                });
+
+                let link = document.createElement('a');
+                link.download = 'sticker-smash.jpeg';
+                link.href = dataUrl;
+                link.click();
+            } catch (e) {
+                console.log(e);
+            }
         }
-    }
+    };
 
     if (status === null) {
         requestPermission();
